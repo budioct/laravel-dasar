39472dafb935960fa425caa1f6558e4d17a5449d
Budhi Octaviansyah <budioct@gmail.com>
2024-05-19 12:51:07 +0700

    Request Lifecycle laravel

--- public/index.php
+++ public/index.php
@@ -46,7 +46,39 @@ require __DIR__.'/../vendor/autoload.php';
 
 $app = require_once __DIR__.'/../bootstrap/app.php';
 
-$kernel = $app->make(Kernel::class);
+/**
+ * Request Lifecycle
+ * ● Sebelum kita membuat kode program menggunakan Laravel, ada baiknya kita perlu tahu cara kerja
+ *   Laravel itu sendiri
+ * ● Terutama bagaimana alur hidup dari request yang kita lakukan ke aplikasi Laravel
+ * ● Jika teman-teman sudah mengikuti kelas PHP MVC yang saya buat, harusnya tidak akan terlalu
+ *   bingung, karena hampir sama cara kerja nya
+ *
+ * public/index.php
+ * ● Entry point pertama dari aplikasi Laravel adalah sebuah file index.php yang terdapat di folder
+ *   public
+ * ● Semua request yang masuk ke aplikasi Laravel, maka akan masuk melalui file ini
+ * ● File ini sengaja disimpan di dalam folder public tersendiri, agar file-file kode program lainnya tidak
+ *   bisa diakses via URL
+ * ● Ini file index.php sebenarnya tidak ada yang kompleks, hanya me-load framework Laravel, dan
+ *   menjalankan kode program yang kita buat
+ *
+ * Kernel
+ * Dari ../public/index.php, request akan dilanjutkan ke class Kernel
+ * class Kernel handle HTTP Kernel, dan Console Kernel
+ * ketika request masuk dari web HTTP maka request akan dilanjutkan ke HTTP kernel.  lokasi file class ../app/Http/Kernel.php
+ *
+ * Service Provider
+ * ● Kernel sendiri sebenarnya adalah core dari logic aplikasi, dimana di dalam Kernel, request yang
+ *   masuk di tangani sampai mendapatkan response
+ * ● Kernel melakukan beberapa hal, pertama Kernel melakukan proses bootstraping, yaitu me-load
+ *   yang namanya Service Provider
+ * ● Laravel akan melakukan iterasi semua Service Provider dan melakukan proses registrasi dan juga
+ *   bootstraping untuk semua Service Provider
+ * ● Service Provider ini lah yang bertanggung jawab melakukan bootstraping semua komponen di
+ *   Laravel, seperti database, queue, validation, routing dan lain-lain
+ */
+$kernel = $app->make(Kernel::class); // initialize
 
 $response = $kernel->handle(
     $request = Request::capture()
